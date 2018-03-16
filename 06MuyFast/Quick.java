ca1e64409a07929e39a96fb21365c107fc4d0641
Sarar Aseer <saseer@stuy.edu>
2018-03-15 22:30:16 -0400

    Evolved into dutch way

--- 06MuyFast/Quick.java
+++ 06MuyFast/Quick.java
@@ -1,54 +1,74 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Quick{
     
     public static void main(String[] args){
-	int[] test = {1,999,9999,999,999,999,999,999,999};
-	System.out.println(partition(test, 0, test.length-1));
-	System.out.println(toString(test));
+	try{
+	    int[] test = new int[Integer.parseInt(args[0])];
+	    for(int i =0; i < test.length; i++){
+		Random seed= new Random();
+		int num=seed.nextInt((Integer.parseInt(args[1])));
+		if(seed.nextBoolean()){
+		    num*=-1;
+		}
+		test[i]=num;
+
+	    }
+	    System.out.println("Before:"+"\n"+toString(test));
+	    System.out.println(partition(test, 0, test.length-1));
+	    System.out.println("After:"+"\n"+toString(test));
+	}
+	catch(Exception IndexOutOfBounds){
+	    System.out.println("After the file name insert the size of the array and the bounds for the numbers in the array"+"\n"+"EX: FileName 10 200"+"\n"+"Would give an array of size 10 with numbers ranging from -200 to 200");
+	}
+    
     }
 
     private static String toString(int[] list){
-	String s="";
-	for (int i = 0; i < list.length; i++){
-	    s+=""+list[i]+",";
+	String s="[";
+	for (int i = 0; i < list.length-1; i++){
+		s+=""+list[i]+",";	    	    
 	}
+	
+	s+=""+list[list.length-1]+"]";
 	return s;
     }
    
     public static int partition(int[] x,int lo, int hi){
-	System.out.println(""+lo+","+hi);
 	Random seed= new Random();
 	int piv=seed.nextInt((hi-lo));
+	System.out.println(toString(x));
 	swap(piv+lo,lo,x);
-	final int num=lo;
-	lo+=1;
-	while(lo<=hi){
-	    System.out.println(""+lo+","+hi);
+	int i=lo+1;
+	int a=x[lo];
+	while(i<=hi){
 	    System.out.println(toString(x));
-	    if(x[lo]> x[num]){
-		swap(lo,hi,x);
+	    if(x[i]< x[lo]){
+		swap(i,lo,x);
+		i++;
+		lo++;
+	    }
+	    else if(x[i]>x [lo]){
+		swap(i,hi,x);
 		hi--;
 	    }
-	    if(x[lo]<x [num]){
-		lo++;
-	    }
 
-	    if(x[lo]==x[num]){
-		lo++;
+	    else if(x[i]==x[lo]){
+		i++;
 	    }
 	      
 	}
-	swap(hi,num,x);
 	System.out.println(""+lo+","+hi);
-	return x[hi];
+	System.out.println("piv:"+a);
+	return x[i];
     }
 
     public static void swap(int a, int b, int list[]){
-	System.out.println(""+a+","+b);
         int temp = list[a];
         list[a] = list[b];
         list[b] = temp;
     }
 }
+

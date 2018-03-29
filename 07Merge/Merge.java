fe3fc8acef0851c6b48260fd99c5603a7e87592c
Sarar Aseer <saseer@stuy.edu>
2018-03-29 00:50:46 -0400

    had wrong file

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -3,42 +3,83 @@ import java.io.*;
 
 public class Merge{
 
-    public static void main(String[] args){
-	int[] test = new int[Integer.parseInt(args[0])];
-	for(int i =0; i < test.length; i++){
-	    Random seed= new Random();
-	    int num=seed.nextInt((Integer.parseInt(args[1])));
-	    if(seed.nextBoolean()){
-		num*=-1;
+    private static final int INCREASE = 0;
+    private static final int DECREASE = 1;
+    private static final int STANDARD = 2;
+    private static final int SMALL_RANGE = 3;
+    private static final int EMPTY = 4;
+
+    private static String name(int i){
+	if(i==0)return "Increassing";
+	if(i==1)return "Decreassing";
+	if(i==2)return "Normal Random";
+	if(i==3)return "Random with Few Values";
+	if(i==4)return "size 0 array";
+	return "Error stat array";
+
+    }
+
+    private static int create(int min, int max){
+	return min + (int)(Math.random()*(max-min));
+    }
+
+    private static int[]makeArray(int size,int type){
+	int[]ans =new int[size];
+	if(type == STANDARD){
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(-1000000,1000000);
 	    }
-	    test[i]=num;
 	}
-
-	System.out.println("Test:"+"\n"+toString(test));
-	mergesort(test);
-	System.out.println("Test:"+"\n"+toString(test));
-	   
+	if(type == INCREASE){
+	    int current = -5 * size;
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(current,current + 10);
+		current += 10;
+	    }
+	}
+	if(type == DECREASE){
+	    int current = 5 * size;
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(current,current + 10);
+		current -= 10;
+	    }
+	}
+	if(type == SMALL_RANGE){
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(-5,5);
+	    }
+	}
+	if(type == EMPTY){
+	    ans = new int[0];
+	}
+	return ans;
     }
 
-    private static void insertionSort(int[] data,int lo, int hi){
-	if (hi > lo){
-      
-	    insertionSort( data, lo,hi-1 );
-      
-	    int pos1 = data[hi-1];
-	    int pos2 = hi-2;
-      	    while (pos2 >= lo && data[pos2] > pos1){
-		    data[pos2+1] = data[pos2];
-		    pos2--;
-		}
-	    data[pos2+1] = pos1;
-	}
-    }
+    public static void main(String[]args){
+	if(args.length < 2)return;
     
+	int size =  Integer.parseInt(args[0]);
+	int type =   Integer.parseInt(args[1]);
+
+	int [] start = makeArray(size,type);
+	int [] result = Arrays.copyOf(start,start.length);
+	Arrays.sort(result);
+    
+	long startTime = System.currentTimeMillis();
+	mergesort(start);
+	long elapsedTime = System.currentTimeMillis() - startTime;
+	if(Arrays.equals(start,result)){
+	    System.out.println("PASS Case "+name(type)+" array, size:"+size+" "+elapsedTime/1000.0+"sec ");
+	}else{
+	    System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+	}
+    }
+
+
     public static String toString(int[] list){
 	String s="[";
 	for (int i = 0; i < list.length-1; i++){
-	    s+=""+list[i]+",";	    	    
+		s+=""+list[i]+",";	    	    
 	}
 	
 	s+=""+list[list.length-1]+"]";
@@ -52,17 +93,17 @@ public class Merge{
 
 
 	}
-	msort(data,temp, 0, data.length);
+	msort(data,temp, 0, data.length-1);
     }
 
+ 
   
     private static void msort(int [] data, int [] temp, int lo, int hi){
-	if(hi-lo<=35){
+	if(hi-lo<=30){
 	    insertionSort(data,lo,hi);
-
 	}
-	else{
-	    int mid = hi/2+lo/2;
+	else if(lo<hi){
+	    int mid = (hi+lo)/2;
 	    msort(data,temp,lo,mid);
 	    msort(data,temp,mid+1,hi);
 	    merge(data,temp,lo,mid,hi);
@@ -71,9 +112,23 @@ public class Merge{
 
 	    }
 	}
-    
 	
     }
+
+   private static void insertionSort(int[] data,int lo, int hi){
+	if (hi > lo){
+      
+	    insertionSort( data, lo,hi-1 );
+      
+	    int pos1 = data[hi-1];
+	    int pos2 = hi-2;
+      	    while (pos2 >= lo && data[pos2] > pos1){
+		    data[pos2+1] = data[pos2];
+		    pos2--;
+		}
+	    data[pos2+1] = pos1;
+	}
+   }
 
     
     
@@ -94,18 +149,16 @@ public class Merge{
 	    pos++;
 	}
 	while (lo <= mid){
-	    x[pos] = y[lo];
-	    lo++;
-	    pos++;
-	}
+            x[pos] = y[lo];
+            lo++;
+            pos++;
+        }
 	while (m2 <=hi){
-	    x[pos] = y[m2];
-	    m2++;
-	    pos++;
-	}
+            x[pos] = y[m2];
+            m2++;
+            pos++;
+        }
     }
-
-
    
-    
+
 }

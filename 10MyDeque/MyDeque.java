2a6c92ac180054ce19ed7ed7066ae7457c17ccdc
Sarar Aseer <saseer@stuy.edu>
2018-04-19 19:06:54 -0400

    Finishing touches

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,24 +1,40 @@
 public class MyDeque<E>{
     private E[] data;
-    private  int  front;
-    private  int  back;
+    private  static int  front;
+    private  static int  back;
     private int size;
     
     public static void main(String[] args){
 	MyDeque<Integer> x= new MyDeque<>(2);
 	x.addFirst(10);
-	System.out.println(x.getFirst());
+	System.out.println(""+front+","+back);
+	System.out.println(x);
 	x.addFirst(412);
-	System.out.println(x.getFirst());
+	System.out.println(""+front+","+back);
+	System.out.println(x);
 	x.addFirst(33);
-	System.out.println(x.getFirst());
+	System.out.println(""+front+","+back);
+	System.out.println(x);
 	x.addLast(23);
-	x.addLast(23);
-	x.addLast(23);
-	x.addLast(23);
-	x.addLast(23);
-		
+	x.addLast(399);
+	x.addLast(13);
+	x.addLast(68);
+	x.addLast(71);
+	System.out.println(""+front+","+back);
+	System.out.println(x);
+	x.addFirst(1212);
+	System.out.println(""+front+","+back);
+	System.out.println(x);
+    }
 
+    public String toString(){
+	String x="";
+	for(int i=0; i < data.length-1; i++){
+	    x+=data[i]+",";
+	}
+	x+=data[data.length-1];
+	return x;
+	
     }
     
     
@@ -26,7 +42,7 @@ public class MyDeque<E>{
     public MyDeque(){
 	data =(E[])new Object[10];
 	front=0;
-	back=0;
+	back=1;
 	size=0;
     }
     @SuppressWarnings("unchecked")
@@ -34,7 +50,7 @@ public class MyDeque<E>{
      public MyDeque(int x){
 	data =(E[])new Object[x];
 	front=0;
-	back=0;
+	back=1;
 	size=0;
     }
 
@@ -50,15 +66,12 @@ public class MyDeque<E>{
 	    resize();	   
 	}
 	if(front<0){
-	    if(front+data.length>back){
-		data[front+data.length]=x;
-	    }	   
+	    data[data.length+front]=x;  
 	}
 	else{
 	    data[front]=x;
 	}
 	front--;
-	back++;
 	size++;
     }
     
@@ -68,7 +81,9 @@ public class MyDeque<E>{
 	}
 	if(size==data.length){
 	    resize();
-	    data[back]=x;
+	}
+	if(back>data.length-1){
+	    data[back-data.length-1]=x;
 	}
 	else{
 	    data[back]=x;
@@ -80,38 +95,47 @@ public class MyDeque<E>{
     public E removeFirst(){
 	front++;	
 	size--;
-	if (front-1<0){
-	    return data[front-1+data.length];
+	if (front<0){	    
+	    return data[front+data.length];
 	}
-	return data[front-1];
+	return data[front];
     }
     
 
     public E removeLast(){
 	back--;
 	size--;
-	return data[back+1];
+	if(back>data.length-1){
+	    return data[back-data.length-1];
+	}
+	return data[back];
     }
 
     public E getFirst(){
-	int x=front+1;
-	if(x<0){
-	    return data[data.length+x];
+	if(front<0){
+	    return data[data.length+front];
 	}
-	return data[x];
+	return data[front];
     }
 
     public E getLast(){
-	int x=back-1;
-	return data[x];
+	if(back>data.length-1){
+	    return data[back-data.length-1];
+	}
+   	return data[back];
 
     }
     
     @SuppressWarnings("unchecked")
     private void resize(){	
 	E [] x =(E[])new Object[data.length*2];
-	for(int i =0; i < data.length; i++){
-	    data[i]=x[i];
+	for(int i =back-1; i >front; i--){
+	    if(i>=0){
+		x[i]=data[i];
+	    }
+	    else{
+		x[i+x.length]=data[i+data.length];
+	    }
 	}
 	data=x;
     }
